9645c6fd56dbaa33ddd9b4ce684d433e83e91ad8
Sashik94 <Sashik94@users.noreply.github.com>
2020-08-01 15:43:50 +0300

    Initial Commit

---
+++ Dog breeds/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Dog breeds
+//
+//  Created by Александр Осипов on 01.08.2020.
+//  Copyright © 2020 Александр Осипов. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
